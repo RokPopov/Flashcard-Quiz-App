af7be3d6e9b5315a3db871addcf99f169028cc31
rokpopov <rokpopov90@gmail.com>
2020-11-02 19:31:25 +0100

    Initialize axios library, fetch data from api, parse JSON response, display in flashcards, style flashcards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FlashcardList from "./components/FlashcardList/index";
 import "./style.css";
+import axios from "axios";
 
 function App() {
 const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+useEffect(() => {
+   axios.get("https://opentdb.com/api.php?amount=10")
+   .then(res => {
+     setFlashcards(res.data.results.map((questionItem, i) => {
+       const answer = decodeString(questionItem.correct_answer)
+       const options = [
+         ...questionItem.incorrect_answers.map(ans => decodeString(ans)), 
+         answer
+      ]
+       return {
+         id: `${i} - ${Date.now()}`,
+         question: decodeString(questionItem.question),
+         answer: answer,
+         options: options.sort(() => Math.random() - 0.5)
+       }
+     }))
+     console.log("response", res.data)
+   })
+}, [])
+
+function decodeString(str) {
+  const textArea = document.createElement("textArea")
+  textArea.innerHTML = str
+  return textArea.value
+}
+
   return (
+    <div className="container">
     <FlashcardList flashcards={flashcards} />
+    </div>
   );
 }
 
