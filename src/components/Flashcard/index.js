3ab86fd06cc425d0fe1d25bcb27291fe3ddb2c41
rokpopov <rokpopov90@gmail.com>
2020-11-02 17:15:31 +0100

    Implement toggle logic to Flashcard component

--- src/components/Flashcard/index.js
+++ src/components/Flashcard/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Flashcard(props) {
+const [toggle, setToggle] = useState(false)
+
   return (
-    <div>
-      {props.flashcard.question}
+    <div onClick={() => setToggle(!toggle)}>
+      {toggle ? props.flashcard.answer : props.flashcard.question}
     </div>
   )
 }
