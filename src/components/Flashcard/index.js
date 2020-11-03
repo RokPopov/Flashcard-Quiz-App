c8a241fc30ab477f410a6b17e927bdf36f6c00de
rokpopov <rokpopov90@gmail.com>
2020-11-03 17:01:03 +0100

    Create search functionality for searching categories and setting amount of flashcards to be displayed

--- src/components/Flashcard/index.js
+++ src/components/Flashcard/index.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function Flashcard(props) {
 const [toggle, setToggle] = useState(false)
+const [height, setHeight] = useState("initial")
+
+const frontElement = useRef();
+const backElement = useRef();
+
+function setMaxHeight() {
+  const frontHeight = frontElement.current.getBoundingClientRect().height;
+  const backHeight = backElement.current.getBoundingClientRect().height;
+  setHeight(Math.max(frontHeight, backHeight, 300))
+}
+
+useEffect(setMaxHeight, [props.flashcard.question, props.flashcard.answer, props.flashcard.options])
+useEffect(() => {
+  window.addEventListener('resize', setMaxHeight)
+  return () => window.removeEventListener('resize', setMaxHeight)
+}, [])
 
   return (
     <div
     className={`card ${toggle ? 'toggle' : ''}`}
+    style={{ height: height }}
     onClick={() => setToggle(!toggle)}
     >
-      <div className="front">
+      <div className="front" ref={frontElement}>
         {props.flashcard.question}
         <div className="flashcard-options">
           {props.flashcard.options.map((option, i) => {
@@ -16,7 +33,7 @@ const [toggle, setToggle] = useState(false)
           })}
         </div>
       </div>
-        <div className="back">{props.flashcard.answer}</div>
+        <div className="back" ref={backElement}>{props.flashcard.answer}</div>
     </div>
   )
 }
